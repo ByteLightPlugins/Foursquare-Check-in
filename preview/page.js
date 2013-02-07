f0f737f31c8afab4e119a990f8666228d956a32d
ByteLight Plugin Repositories <plugins@bytelight.com>
2013-02-06 18:17:49 -0800

    Saved at Wed Feb 06 2013 21:17:47 GMT-0500 (EST)

---
+++ preview/page.js
@@ -0,0 +1,10 @@
+$(function() {
+  data = JSON.parse(BL.getContentForPreview().content[0].data);
+  $('span').text(data.venueName)
+  if (BL.foursquareAuthenticated()) {
+    BL.foursquareCheckin(data.venueId, data.oncePerSession, function() {
+      $('#checkin').text('You just checked in with foursquare at: ' + data.venueName);
+    })
+  }
+  BL.previewReady(); // this is what tells the preview page to display this component - must be called
+})
